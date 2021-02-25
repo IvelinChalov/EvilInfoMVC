32c2b565d1b7b5ed80f386d4bb9bcb56083476d4
Ivelin Chalov <ivelin_chalov@abv.bg>
2021-02-25 19:32:45 +0200

    Removig some loops

--- EvilInfo/Program.cs
+++ EvilInfo/Program.cs
@@ -2,36 +2,29 @@
 using EvilInfo.Presentation.Menus;
 using EvilInfo.Services;
 using EvilInfo.Services.DAO;
-using System;
 
 namespace EvilInfo
 {
-    [System.Runtime.InteropServices.Guid("E4F7C2AA-0660-4562-8E07-C67F8E08DD6F")]
-    class Program
-    {
-        static void Main(string[] args)
-        {
-			for (int i = 0; i < 10; i++)
-			{
-				Console.WriteLine("ddf");
-			}
+	[System.Runtime.InteropServices.Guid("E4F7C2AA-0660-4562-8E07-C67F8E08DD6F")]
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			EvilInfoDBContext context = new EvilInfoDBContext();
+			CountryDAO countryDAO = new CountryDAO(context);
+			HomeDAO homeDAO = new HomeDAO(context);
+			RoleDAO role = new RoleDAO(context);
+			TownDAO townDAO = new TownDAO(context);
+			VillainDAO villainDAO = new VillainDAO(context);
+			RoleDAO roleDAO = new RoleDAO(context);
+			MinionDAO minionDAO = new MinionDAO(context);
+			JobDAO jobDAO = new JobDAO(context);
 
+			HomeController homeController = new HomeController(homeDAO, villainDAO, countryDAO, townDAO, roleDAO);
+			MinionController minionController = new MinionController(minionDAO, townDAO);
+			VillainController villainController = new VillainController(villainDAO, jobDAO);
 
-            EvilInfoDBContext context = new EvilInfoDBContext();
-            CountryDAO countryDAO = new CountryDAO(context);
-            HomeDAO homeDAO = new HomeDAO(context);
-            RoleDAO role = new RoleDAO(context); 
-            TownDAO townDAO = new TownDAO(context);
-            VillainDAO villainDAO = new VillainDAO(context);
-            RoleDAO roleDAO = new RoleDAO(context);
-            MinionDAO minionDAO = new MinionDAO(context);
-            JobDAO jobDAO = new JobDAO(context);
-
-            HomeController homeController = new HomeController(homeDAO, villainDAO, countryDAO, townDAO, roleDAO);
-            MinionController minionController = new MinionController(minionDAO, townDAO);
-            VillainController villainController = new VillainController(villainDAO, jobDAO);
-
-            HomeMenu homeMenu = new HomeMenu(homeController, minionController, villainController);
-        }
-    }
+			HomeMenu homeMenu = new HomeMenu(homeController, minionController, villainController);
+		}
+	}
 }
