a2a9e3a5136719908a9faf5b80e5e62a4f2c0da1
Ivelin Chalov <ivelin_chalov@abv.bg>
2021-02-27 11:10:33 +0200

    Small changes and refactoring

--- EvilInfo.Services/DAO/HomeDAO.cs
+++ EvilInfo.Services/DAO/HomeDAO.cs
@@ -1,133 +1,132 @@
 ﻿using EvilInfo.Services.Models;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace EvilInfo.Services.DAO
 {
-    public class HomeDAO
-    {
-        //public string GetUserRole(string username, string password)
-        //{
-        //    string userRole = string.Empty;
-        //    SqlConnection dbConnection = new SqlConnection(this.connectionString);
-        //    dbConnection.Open();
+	public class HomeDAO
+	{
+		//public string GetUserRole(string username, string password)
+		//{
+		//    string userRole = string.Empty;
+		//    SqlConnection dbConnection = new SqlConnection(this.connectionString);
+		//    dbConnection.Open();
 
-        //    using (dbConnection)
-        //    {
-        //        SqlCommand command = new SqlCommand(
-        //        "SELECT [Roles].[Name]" +
-        //        "FROM[EvilInfo].[dbo].[Users]" +
-        //        "INNER JOIN[Logins] ON[Logins].[Id] = [Users].[Id]" +
-        //        "INNER JOIN [Roles] ON [Roles].[Id] = [Users].[RoleId]" +
-        //        "WHERE[Logins].[Username] = @username " +
-        //        "AND" +
-        //        "[Logins].[Password] = @password", dbConnection);
+		//    using (dbConnection)
+		//    {
+		//        SqlCommand command = new SqlCommand(
+		//        "SELECT [Roles].[Name]" +
+		//        "FROM[EvilInfo].[dbo].[Users]" +
+		//        "INNER JOIN[Logins] ON[Logins].[Id] = [Users].[Id]" +
+		//        "INNER JOIN [Roles] ON [Roles].[Id] = [Users].[RoleId]" +
+		//        "WHERE[Logins].[Username] = @username " +
+		//        "AND" +
+		//        "[Logins].[Password] = @password", dbConnection);
 
-        //        command.Parameters.AddWithValue("@username", username);
-        //        command.Parameters.AddWithValue("@password", password);
+		//        command.Parameters.AddWithValue("@username", username);
+		//        command.Parameters.AddWithValue("@password", password);
 
-        //        SqlDataReader reader = command.ExecuteReader();
+		//        SqlDataReader reader = command.ExecuteReader();
 
-        //        using (reader)
-        //        {
-        //            while (reader.Read())
-        //            {
-        //                userRole = reader["Name"].ToString();
-        //            }
-        //        }
+		//        using (reader)
+		//        {
+		//            while (reader.Read())
+		//            {
+		//                userRole = reader["Name"].ToString();
+		//            }
+		//        }
 
-        //        return userRole;
-        //    }
-        //}
+		//        return userRole;
+		//    }
+		//}
 
-        public Users LogIn(string username, string password)
-        {
-            var user = this.context.Logins
-                .Include(l => l.Users)
-                .ThenInclude(u => u.Role)
-                .Where(u => u.Username.Equals(username) && u.Password.Equals(password))
-                .FirstOrDefault().Users;
 
-            return user;
-        }
+		public Users LogIn(string username, string password)
+		{
+			var user = this.context.Logins
+				.Include(l => l.Users)
+				.ThenInclude(u => u.Role)
+				.Where(u => u.Username.Equals(username) && u.Password.Equals(password))
+				.FirstOrDefault().Users;
 
-        public void RegisterUser(Logins loginInfo)
-        {
-            this.context.Logins.Add(loginInfo);
-            this.context.SaveChanges();
-            
-        }
+			return user;
+		}
 
-        //Как бих могъл да спестя писането на това като в базата вече преизползвам сторнатата
-        public void RegisterVillain(VillainUsers user, Logins loginInfo)
-        {
-            this.context.Logins.Add(loginInfo);
-            this.context.VillainUsers.Add(user);
-            this.context.SaveChanges();
-        }
+		public void RegisterUser(Logins loginInfo)
+		{
+			this.context.Logins.Add(loginInfo);
+			this.context.SaveChanges();
 
-        //public int GetUserId(string username, string password)
-        //{
-        //    SqlConnection dbConnection = new SqlConnection(this.connectionString);
-        //    int userId = -1;
-        //    dbConnection.Open();
+		}
 
-        //    using (dbConnection)
-        //    {
-        //        SqlCommand command = new SqlCommand(
-        //        "SELECT [Logins].[Id]" +
-        //        " FROM [Logins]" +
-        //        " WHERE [Logins].[Username] = @username" +
-        //        " AND" +
-        //        " [Logins].[Password] = @password", dbConnection);
+		//Как бих могъл да спестя писането на това като в базата вече преизползвам сторнатата
+		public void RegisterVillain(VillainUsers user, Logins loginInfo)
+		{
+			this.context.Logins.Add(loginInfo);
+			this.context.VillainUsers.Add(user);
+			this.context.SaveChanges();
+		}
 
-        //        command.Parameters.AddWithValue("@username", username);
-        //        command.Parameters.AddWithValue("@password", password);
+		//public int GetUserId(string username, string password)
+		//{
+		//    SqlConnection dbConnection = new SqlConnection(this.connectionString);
+		//    int userId = -1;
+		//    dbConnection.Open();
 
-        //        SqlDataReader reader = command.ExecuteReader();
+		//    using (dbConnection)
+		//    {
+		//        SqlCommand command = new SqlCommand(
+		//        "SELECT [Logins].[Id]" +
+		//        " FROM [Logins]" +
+		//        " WHERE [Logins].[Username] = @username" +
+		//        " AND" +
+		//        " [Logins].[Password] = @password", dbConnection);
 
-        //        using (reader)
-        //        {
-        //            while (reader.Read())
-        //            {
-        //                userId = Convert.ToInt32(reader["Id"]);
-        //            }
-        //        }
+		//        command.Parameters.AddWithValue("@username", username);
+		//        command.Parameters.AddWithValue("@password", password);
 
-        //        return userId;
-        //    }
-        //}
+		//        SqlDataReader reader = command.ExecuteReader();
 
-        //public bool IsUsernameFree(string username)
-        //{
-        //    SqlConnection dbConnection = new SqlConnection(this.connectionString);
+		//        using (reader)
+		//        {
+		//            while (reader.Read())
+		//            {
+		//                userId = Convert.ToInt32(reader["Id"]);
+		//            }
+		//        }
 
-        //    dbConnection.Open();
+		//        return userId;
+		//    }
+		//}
 
-        //    using (dbConnection)
-        //    {
-        //        SqlCommand command = new SqlCommand(
-        //        "SELECT [Logins].[Username]" +
-        //        " FROM [Logins]" +
-        //        " WHERE [Logins].[Username] = @username", dbConnection);
+		//public bool IsUsernameFree(string username)
+		//{
+		//    SqlConnection dbConnection = new SqlConnection(this.connectionString);
 
-        //        command.Parameters.AddWithValue("@username", username);
+		//    dbConnection.Open();
 
-        //        SqlDataReader reader = command.ExecuteReader();
+		//    using (dbConnection)
+		//    {
+		//        SqlCommand command = new SqlCommand(
+		//        "SELECT [Logins].[Username]" +
+		//        " FROM [Logins]" +
+		//        " WHERE [Logins].[Username] = @username", dbConnection);
 
-        //        return reader.HasRows;
-        //    }
-        //}
+		//        command.Parameters.AddWithValue("@username", username);
 
-        private EvilInfoDBContext context;
-        public HomeDAO(EvilInfoDBContext context)
-        {
-            if (context == null) throw new ArgumentNullException("context");
+		//        SqlDataReader reader = command.ExecuteReader();
 
-            this.context = context;
-        }
-    }
+		//        return reader.HasRows;
+		//    }
+		//}
+
+		private EvilInfoDBContext context;
+		public HomeDAO(EvilInfoDBContext context)
+		{
+			if (context == null) throw new ArgumentNullException("context");
+
+			this.context = context;
+		}
+	}
 }
--- EvilInfo/Presentation/Menus/HomeMenu.cs
+++ EvilInfo/Presentation/Menus/HomeMenu.cs
@@ -8,168 +8,168 @@ using System.Text;
 
 namespace EvilInfo.Presentation.Menus
 {
-    class HomeMenu
-    {
-        private readonly float CloseCommand = 3;
-        private HomeController homeController;
-        private MinionController minionController;
-        private VillainController villainController;
+	class HomeMenu
+	{
+		private readonly float CloseCommand = 3;
+		private HomeController homeController;
+		private MinionController minionController;
+		private VillainController villainController;
 
-        public void Input()
-        {
-            int operation = -1;
-            do
-            {
-                ShowMenu();
-                if (!int.TryParse(Console.ReadLine(), out operation)) throw new FormatException("Value must be integer nuber!");
-                switch (operation)
-                {
-                    case 1:
-                        Console.Clear();
-                        LoginUser();
-                        break;
-                    case 2:
-                        RegisterUser();
-                        break;
-                    default:
-                        break;
-                }
-            } while (operation != CloseCommand);
-        }
+		public void Input()
+		{
+			int operation = -1;
+			do
+			{
+				ShowMenu();
+				if (!int.TryParse(Console.ReadLine(), out operation)) throw new FormatException("Value must be integer nuber!");
+				switch (operation)
+				{
+					case 1:
+						Console.Clear();
+						LoginUser();
+						break;
+					case 2:
+						RegisterUser();
+						break;
+					default:
+						break;
+				}
+			} while (operation != CloseCommand);
+		}
 
-        private void RegisterUser()
-        {
-            RegistrationViewModel registrationViewModel = new RegistrationViewModel();
+		private void RegisterUser()
+		{
+			RegistrationViewModel registrationViewModel = new RegistrationViewModel();
 
-            Console.Write("Username: ");
-            string username = Console.ReadLine();
-            Console.Write("Password: ");
-            string password = Console.ReadLine();
-            Console.Write("Repeat password: ");
-            string repPasswod = Console.ReadLine();
-            if (!IsSame(password, repPasswod)) throw new Exception("Password mismatch");
+			Console.Write("Username: ");
+			string username = Console.ReadLine();
+			Console.Write("Password: ");
+			string password = Console.ReadLine();
+			Console.Write("Repeat password: ");
+			string repPasswod = Console.ReadLine();
+			if (!IsSame(password, repPasswod)) throw new Exception("Password mismatch");
 
-            Console.Write("First name: ");
-            string firstName = Console.ReadLine();
-            if (!Validator.IsStrigValid(firstName)) throw new FormatException("Invalid charecter for first name");
-            Console.Write("Last name: ");
-            string lastName = Console.ReadLine();
-            if (!Validator.IsStrigValid(lastName)) throw new FormatException("Invalid charecter for last name");
-            Console.Write("Country name: ");
-            string countryName = Console.ReadLine();
-            if (!Validator.IsStrigValid(countryName)) throw new FormatException("Invalid charecter for country name");
-            Console.Write("Town name: ");
-            string townName = Console.ReadLine();
-            if (!Validator.IsStrigValid(townName)) throw new FormatException("Invalid charecter for town name");
-            Console.Write("Role name: ");
-            string roleName = Console.ReadLine();
-            if (!Validator.IsStrigValid(roleName)) throw new FormatException("Invalid charecter for role name");
+			Console.Write("First name: ");
+			string firstName = Console.ReadLine();
+			if (!Validator.IsStrigValid(firstName)) throw new FormatException("Invalid charecter for first name");
+			Console.Write("Last name: ");
+			string lastName = Console.ReadLine();
+			if (!Validator.IsStrigValid(lastName)) throw new FormatException("Invalid charecter for last name");
+			Console.Write("Country name: ");
+			string countryName = Console.ReadLine();
+			if (!Validator.IsStrigValid(countryName)) throw new FormatException("Invalid charecter for country name");
+			Console.Write("Town name: ");
+			string townName = Console.ReadLine();
+			if (!Validator.IsStrigValid(townName)) throw new FormatException("Invalid charecter for town name");
+			Console.Write("Role name: ");
+			string roleName = Console.ReadLine();
+			if (!Validator.IsStrigValid(roleName)) throw new FormatException("Invalid charecter for role name");
 
-            registrationViewModel.FirstName = firstName;
-            registrationViewModel.LastName = lastName;
-            registrationViewModel.TownName = townName;
-            registrationViewModel.CountryName = countryName;
-            registrationViewModel.Username = username;
-            registrationViewModel.Password = HashPassword(password);
-            registrationViewModel.RoleName = roleName;
+			registrationViewModel.FirstName = firstName;
+			registrationViewModel.LastName = lastName;
+			registrationViewModel.TownName = townName;
+			registrationViewModel.CountryName = countryName;
+			registrationViewModel.Username = username;
+			registrationViewModel.Password = HashPassword(password);
+			registrationViewModel.RoleName = roleName;
 
-            if (roleName.ToLower().Equals("villain"))
-            {
-                Console.Write("Evilness factor:");
-                registrationViewModel.EvilnessFactor = Console.ReadLine();
-                if (!Validator.IsStrigValid(registrationViewModel.EvilnessFactor)) throw new FormatException("Invalid  evilnessFactor");                
-            }
+			if (roleName.ToLower().Equals("villain"))
+			{
+				Console.Write("Evilness factor:");
+				registrationViewModel.EvilnessFactor = Console.ReadLine();
+				if (!Validator.IsStrigValid(registrationViewModel.EvilnessFactor)) throw new FormatException("Invalid  evilnessFactor");
+			}
 
-            this.homeController.Register(registrationViewModel);
-        }
+			this.homeController.Register(registrationViewModel);
+		}
 
-        private string HashPassword(string password)
-        {
-            var provider = new SHA1CryptoServiceProvider();
-            var encoding = new UnicodeEncoding();
-            return Convert.ToBase64String(provider.ComputeHash(encoding.GetBytes(password)));
-        }
+		private string HashPassword(string password)
+		{
+			var provider = new SHA1CryptoServiceProvider();
+			var encoding = new UnicodeEncoding();
+			return Convert.ToBase64String(provider.ComputeHash(encoding.GetBytes(password)));
+		}
 
-        private bool IsSame(string first, string second)
-        {
-            return string.Equals(first, second);
-        }
+		private bool IsSame(string first, string second)
+		{
+			return string.Equals(first, second);
+		}
 
-        private void Redirect(int userId, string userRole)
-        {
-            switch (userRole)
-            {
-                case "Minion":
-                    {
-                        MinionMenu m = new MinionMenu(this.minionController, userId);
-                    }
-                    break;
-                case "Villain":
-                    {
-                       VillainMenu v = new VillainMenu(this.villainController, userId);
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
+		private void Redirect(int userId, string userRole)
+		{
+			switch (userRole)
+			{
+				case "Minion":
+					{
+						MinionMenu m = new MinionMenu(this.minionController, userId);
+					}
+					break;
+				case "Villain":
+					{
+						VillainMenu v = new VillainMenu(this.villainController, userId);
+					}
+					break;
+				default:
+					break;
+			}
+		}
 
-        private void LoginUser()
-        {
-            Console.Write("Username: ");
-            string username = Console.ReadLine();
-            Console.Write("Password: ");
-            string password = HashPassword(Console.ReadLine());
+		private void LoginUser()
+		{
+			Console.Write("Username: ");
+			string username = Console.ReadLine();
+			Console.Write("Password: ");
+			string password = HashPassword(Console.ReadLine());
 
-            string userRole;
-            int userId = this.homeController.Login(username, password, out userRole);
+			string userRole;
+			int userId = this.homeController.Login(username, password, out userRole);
 
-            Redirect(userId, userRole);
-        }
+			Redirect(userId, userRole);
+		}
 
-        public void ShowMenu()
-        {
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine(new string(' ', 10) + "WELCOME TO EVIL.INFO" + new string(' ', 18));
-            Console.WriteLine(new string('-', 40));
-            Console.WriteLine("1. Login");
-            Console.WriteLine("2. Register");
-            Console.WriteLine("3. Exit");
-        }
+		public void ShowMenu()
+		{
+			Console.WriteLine(new string('-', 40));
+			Console.WriteLine(new string(' ', 10) + "WELCOME TO EVIL.INFO" + new string(' ', 18));
+			Console.WriteLine(new string('-', 40));
+			Console.WriteLine("1. Login");
+			Console.WriteLine("2. Register");
+			Console.WriteLine("3. Exit");
+		}
 
-        public HomeMenu(HomeController homeController, MinionController minionController, VillainController villainController)
-        {
-            if (homeController == null) throw new ArgumentNullException("loginController");
-            if (minionController == null) throw new ArgumentNullException("minionController");
-            if (villainController == null) throw new ArgumentNullException("villainController");
+		public HomeMenu(HomeController homeController, MinionController minionController, VillainController villainController)
+		{
+			if (homeController == null) throw new ArgumentNullException("loginController");
+			if (minionController == null) throw new ArgumentNullException("minionController");
+			if (villainController == null) throw new ArgumentNullException("villainController");
 
-            this.homeController = homeController;
-            this.minionController = minionController;
-            this.villainController = villainController;
+			this.homeController = homeController;
+			this.minionController = minionController;
+			this.villainController = villainController;
 
-            try
-            {
-                Input();
-            }
-            catch (IncorectCredentialsException e)
-            {
+			try
+			{
+				Input();
+			}
+			catch (IncorectCredentialsException e)
+			{
 
-                Console.WriteLine(e.Message);
-            }
-            catch (FormatException e)
-            {
+				Console.WriteLine(e.Message);
+			}
+			catch (FormatException e)
+			{
 
-                Console.WriteLine(e.Message);
-            }
-            catch (Exception e)
-            {
+				Console.WriteLine(e.Message);
+			}
+			catch (Exception e)
+			{
 
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                Input();
-            }
-        }
-    }
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				Input();
+			}
+		}
+	}
 }
