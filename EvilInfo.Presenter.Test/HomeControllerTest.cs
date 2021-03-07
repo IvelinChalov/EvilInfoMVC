577394e00f1851589cd326512427ae96a1a24614
Ivelin Chalov <ivelin_chalov@abv.bg>
2021-03-07 08:40:10 +0200

    Adding some unit test with MOQ

--- EvilInfo.Presenter.Test/HomeControllerTest.cs
+++ EvilInfo.Presenter.Test/HomeControllerTest.cs
@@ -6,6 +6,7 @@ using EvilInfo.Services;
 using EvilInfo.Services.DAO;
 using EvilInfo.Services.Models;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using NUnit.Framework;
 using System.Linq;
 
@@ -69,6 +70,35 @@ namespace EvilInfo.Presenter.Test
 		}
 
 		[Test]
+		public void LoginUserWrongeCredentialsThrowsIncorectCredetialsExceptionUsingMOQ()
+		{
+			//Arrange
+			EvilInfoDBContext context = new EvilInfoDBContext();
+			CountryDAO countryDAO = new CountryDAO(context);
+
+			Mock<IHomeDAO> mock = new Mock<IHomeDAO>();
+			mock.Setup(m => m.LogIn(It.IsAny<string>(), It.IsAny<string>()))
+				.Returns(() => null);
+
+			RoleDAO role = new RoleDAO(context);
+			TownDAO townDAO = new TownDAO(context);
+			VillainDAO villainDAO = new VillainDAO(context);
+			RoleDAO roleDAO = new RoleDAO(context);
+			MinionDAO minionDAO = new MinionDAO(context);
+			JobDAO jobDAO = new JobDAO(context);
+
+			HomeController homeController = new HomeController(mock.Object, villainDAO, countryDAO, townDAO, roleDAO);
+
+			string username = "wrongUsername";
+			string password = "1234";
+
+			//Act/Assert
+			var exception = Assert.Throws<IncorectCredentialsException>(() => homeController.Login(username, password, out _));
+
+			Assert.That(exception.Message, Is.EqualTo("Incorect credentials"));
+		}
+
+		[Test]
 		public void RegisterMinionUserSuccessfully()
 		{
 			//Arrange
@@ -105,6 +135,113 @@ namespace EvilInfo.Presenter.Test
 			context.Users.Remove(loginUser.Users);
 			context.Logins.Remove(loginUser);
 			context.SaveChanges();
+		}
+
+		[Test]
+		public void LoginUserSuccessfullyUsingMOQ()
+		{
+			Mock<IHomeDAO> mock = new Mock<IHomeDAO>();
+			//mock.Setup(m => m.LogIn(It.IsAny<string>(), It.IsAny<string>()))
+			mock.Setup(m => m.LogIn("testUser", "1234"))
+				.Returns(CreateDummyUser());
+
+			EvilInfoDBContext context = new EvilInfoDBContext();
+			CountryDAO countryDAO = new CountryDAO(context);
+			RoleDAO role = new RoleDAO(context);
+			TownDAO townDAO = new TownDAO(context);
+			VillainDAO villainDAO = new VillainDAO(context);
+			RoleDAO roleDAO = new RoleDAO(context);
+			MinionDAO minionDAO = new MinionDAO(context);
+			JobDAO jobDAO = new JobDAO(context);
+
+			HomeController homeController = new HomeController(mock.Object, villainDAO, countryDAO, townDAO, roleDAO);
+
+			string username = "testUser";
+			string password = "1234";
+			int actualUserId = 0;
+
+			int userIdResult = homeController.Login(username, password, out _);
+
+			Assert.That(userIdResult, Is.EqualTo(actualUserId));
+
+		}
+
+		[Test]
+		public void RegisterMinionUserSuccessfullyUsingMOQ()
+		{
+			//Arrange
+			EvilInfoDBContext context = new EvilInfoDBContext();
+			CountryDAO countryDAO = new CountryDAO(context);
+
+			Mock<IHomeDAO> mock = new Mock<IHomeDAO>();
+			mock.Setup(m => m.RegisterUser(It.IsAny<Logins>()));
+
+			RoleDAO role = new RoleDAO(context);
+			TownDAO townDAO = new TownDAO(context);
+			VillainDAO villainDAO = new VillainDAO(context);
+			RoleDAO roleDAO = new RoleDAO(context);
+			MinionDAO minionDAO = new MinionDAO(context);
+			JobDAO jobDAO = new JobDAO(context);
+
+			HomeController homeController = new HomeController(mock.Object, villainDAO, countryDAO, townDAO, roleDAO);
+
+			RegistrationViewModel registrationViewModel = new RegistrationViewModel();
+			registrationViewModel.FirstName = "Ivan";
+			registrationViewModel.LastName = "Ivanov";
+			registrationViewModel.RoleName = "Minion";
+			registrationViewModel.CountryName = "Bulgaria";
+			registrationViewModel.TownName = "Ruse";
+			registrationViewModel.Username = "TestUsername";
+			registrationViewModel.Password = "testPassword";
+
+			homeController.Register(registrationViewModel);
+
+			mock.Verify(m => m.RegisterUser(It.IsAny<Logins>()), Times.Once());
+
+		}
+
+		[Test]
+		public void RegisterVillainUserSuccessfullyUsingMOQ()
+		{
+			//Arrange
+			EvilInfoDBContext context = new EvilInfoDBContext();
+			CountryDAO countryDAO = new CountryDAO(context);
+
+			Mock<IHomeDAO> mock = new Mock<IHomeDAO>();
+			mock.Setup(m => m.RegisterVillain(It.IsAny<VillainUsers>(), It.IsAny<Logins>()));
+
+			RoleDAO role = new RoleDAO(context);
+			TownDAO townDAO = new TownDAO(context);
+			VillainDAO villainDAO = new VillainDAO(context);
+			RoleDAO roleDAO = new RoleDAO(context);
+			MinionDAO minionDAO = new MinionDAO(context);
+			JobDAO jobDAO = new JobDAO(context);
+
+			HomeController homeController = new HomeController(mock.Object, villainDAO, countryDAO, townDAO, roleDAO);
+
+			RegistrationViewModel registrationViewModel = new RegistrationViewModel();
+			registrationViewModel.FirstName = "Ivan";
+			registrationViewModel.LastName = "Ivanov";
+			registrationViewModel.RoleName = "Minion";
+			registrationViewModel.CountryName = "Bulgaria";
+			registrationViewModel.TownName = "Ruse";
+			registrationViewModel.Username = "TestUsername";
+			registrationViewModel.Password = "testPassword";
+			registrationViewModel.EvilnessFactor = "Bad";
+
+			homeController.Register(registrationViewModel);
+
+			mock.Verify(m => m.RegisterVillain(It.IsAny<VillainUsers>(), It.IsAny<Logins>()), Times.Once());
+
+		}
+
+		private Users CreateDummyUser()
+		{
+			Users user = new Users();
+			user.Role = new Roles() { Name = "Minion" };
+			user.Id = 0;
+
+			return user;
 		}
 	}
 }
